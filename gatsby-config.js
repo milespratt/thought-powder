bdd03597c1894efbe2803c9265fb560c76b2d224
Miles Pratt <miles@Miless-MacBook-Air.local>
2020-11-20 15:45:20 -0500

    Change google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,27 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-  /* Your site config here */
   plugins: [
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: "gatsby-plugin-web-font-loader",
       options: {
-        fonts: [
-          {
-            family: "Darker Grotesque",
-            variants: [`300`, `400`, `700`],
-          },
-          {
-            family: "Noto Serif",
-            variants: [`400`, `700`],
-          },
-          {
-            family: "Montserrat",
-            variants: [`300`, `400`, `700`],
-          },
-          {
-            family: "Merriweather",
-            variants: [`300`, `400`, `700`],
-          },
-        ],
+        google: {
+          families: [
+            "Darker Grotesque",
+            "Noto Serif",
+            "Montserrat",
+            "Merriweather",
+          ],
+        },
       },
     },
     `gatsby-plugin-playground`,
